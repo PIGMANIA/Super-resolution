782ba6a6a626d46832f8b163e6a66119f755824d
heaseochung <heaseochung@gmail.com>
2022-07-19 13:29:18 +0900

    Add upsampler in the tail for super-resolution

--- scripts/archs/SCUNet/models.py
+++ scripts/archs/SCUNet/models.py
@@ -31,12 +31,16 @@ class WMSA(nn.Module):
         self.n_heads = input_dim // head_dim
         self.window_size = window_size
         self.type = type
-        self.embedding_layer = nn.Linear(self.input_dim, 3 * self.input_dim, bias=True)
+        self.embedding_layer = nn.Linear(
+            self.input_dim, 3 * self.input_dim, bias=True
+        )
 
         # TODO recover
         # self.relative_position_params = nn.Parameter(torch.zeros(self.n_heads, 2 * window_size - 1, 2 * window_size -1))
         self.relative_position_params = nn.Parameter(
-            torch.zeros((2 * window_size - 1) * (2 * window_size - 1), self.n_heads)
+            torch.zeros(
+                (2 * window_size - 1) * (2 * window_size - 1), self.n_heads
+            )
         )
 
         self.linear = nn.Linear(self.input_dim, self.output_dim)
@@ -122,7 +126,10 @@ class WMSA(nn.Module):
         # Using Attn Mask to distinguish different subwindows.
         if self.type != "W":
             attn_mask = self.generate_mask(
-                h_windows, w_windows, self.window_size, shift=self.window_size // 2
+                h_windows,
+                w_windows,
+                self.window_size,
+                shift=self.window_size // 2,
             )
             sim = sim.masked_fill_(attn_mask, float("-inf"))
 
@@ -183,11 +190,15 @@ class Block(nn.Module):
             self.type = "W"
 
         print(
-            "Block Initial Type: {}, drop_path_rate:{:.6f}".format(self.type, drop_path)
+            "Block Initial Type: {}, drop_path_rate:{:.6f}".format(
+                self.type, drop_path
+            )
         )
         self.ln1 = nn.LayerNorm(input_dim)
         self.msa = WMSA(input_dim, input_dim, head_dim, window_size, self.type)
-        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.drop_path = (
+            DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        )
         self.ln2 = nn.LayerNorm(input_dim)
         self.mlp = nn.Sequential(
             nn.Linear(input_dim, 4 * input_dim),
@@ -275,6 +286,7 @@ class ConvTransBlock(nn.Module):
 class Generator(nn.Module):
     def __init__(self, cfg):
         super(Generator, self).__init__()
+        self.scale = cfg.scale
         self.in_nc = cfg.in_nc
         self.config = cfg.config
         self.dim = cfg.dim
@@ -282,10 +294,12 @@ class Generator(nn.Module):
         self.window_size = cfg.window_size
         self.drop_path_rate = cfg.drop_path_rate
         self.input_resolution = cfg.input_resolution
+        self.upsampler = cfg.upsampler
 
         # drop path rate for each layer
         dpr = [
-            x.item() for x in torch.linspace(0, self.drop_path_rate, sum(self.config))
+            x.item()
+            for x in torch.linspace(0, self.drop_path_rate, sum(self.config))
         ]
 
         self.m_head = [nn.Conv2d(self.in_nc, self.dim, 3, 1, 1, bias=False)]
@@ -394,7 +408,20 @@ class Generator(nn.Module):
             for i in range(self.config[6])
         ]
 
-        self.m_tail = [nn.Conv2d(self.dim, self.in_nc, 3, 1, 1, bias=False)]
+        if self.upsampler == "nearest+conv":
+            assert self.scale > 1
+            self.m_tail = [
+                nn.Conv2d(self.dim, self.dim, 3, 1, 1, bias=False),
+                nn.LeakyReLU(inplace=True),
+            ]
+            self.conv_up1 = nn.Conv2d(self.dim, self.dim, 3, 1, 1)
+            if self.scale == 4:
+                self.conv_up2 = nn.Conv2d(self.dim, self.dim, 3, 1, 1)
+            self.conv_hr = nn.Conv2d(self.dim, self.dim, 3, 1, 1)
+            self.conv_last = nn.Conv2d(self.dim, self.in_nc, 3, 1, 1)
+            self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        else:
+            self.m_tail = [nn.Conv2d(self.dim, self.in_nc, 3, 1, 1, bias=False)]
 
         self.m_head = nn.Sequential(*self.m_head)
         self.m_down1 = nn.Sequential(*self.m_down1)
@@ -422,9 +449,30 @@ class Generator(nn.Module):
         x = self.m_up3(x + x4)
         x = self.m_up2(x + x3)
         x = self.m_up1(x + x2)
-        x = self.m_tail(x + x1)
 
-        x = x[..., :h, :w]
+        if self.upsampler == "nearest+conv":
+            x = self.m_tail(x) + x1
+            x = self.lrelu(
+                self.conv_up1(
+                    torch.nn.functional.interpolate(
+                        x, scale_factor=2, mode="nearest"
+                    )
+                )
+            )
+            if self.scale == 4:
+                x = self.lrelu(
+                    self.conv_up2(
+                        torch.nn.functional.interpolate(
+                            x, scale_factor=2, mode="nearest"
+                        )
+                    )
+                )
+
+            x = self.conv_last(self.lrelu(self.conv_hr(x)))
+            x = x[..., : h * self.scale, : w * self.scale]
+        else:
+            x = self.m_tail(x + x1)
+            x = x[..., :h, :w]
 
         return x
 
@@ -478,8 +526,12 @@ class Discriminator(nn.Module):
             nn.Conv2d(self.num_feat * 2, self.num_feat, 3, 1, 1, bias=False)
         )
         # extra convolutions
-        self.conv7 = norm(nn.Conv2d(self.num_feat, self.num_feat, 3, 1, 1, bias=False))
-        self.conv8 = norm(nn.Conv2d(self.num_feat, self.num_feat, 3, 1, 1, bias=False))
+        self.conv7 = norm(
+            nn.Conv2d(self.num_feat, self.num_feat, 3, 1, 1, bias=False)
+        )
+        self.conv8 = norm(
+            nn.Conv2d(self.num_feat, self.num_feat, 3, 1, 1, bias=False)
+        )
         self.conv9 = nn.Conv2d(self.num_feat, 1, 3, 1, 1)
 
     def forward(self, x):
@@ -490,17 +542,23 @@ class Discriminator(nn.Module):
         x3 = F.leaky_relu(self.conv3(x2), negative_slope=0.2, inplace=True)
 
         # upsample
-        x3 = F.interpolate(x3, scale_factor=2, mode="bilinear", align_corners=False)
+        x3 = F.interpolate(
+            x3, scale_factor=2, mode="bilinear", align_corners=False
+        )
         x4 = F.leaky_relu(self.conv4(x3), negative_slope=0.2, inplace=True)
 
         if self.skip_connection:
             x4 = x4 + x2
-        x4 = F.interpolate(x4, scale_factor=2, mode="bilinear", align_corners=False)
+        x4 = F.interpolate(
+            x4, scale_factor=2, mode="bilinear", align_corners=False
+        )
         x5 = F.leaky_relu(self.conv5(x4), negative_slope=0.2, inplace=True)
 
         if self.skip_connection:
             x5 = x5 + x1
-        x5 = F.interpolate(x5, scale_factor=2, mode="bilinear", align_corners=False)
+        x5 = F.interpolate(
+            x5, scale_factor=2, mode="bilinear", align_corners=False
+        )
         x6 = F.leaky_relu(self.conv6(x5), negative_slope=0.2, inplace=True)
 
         if self.skip_connection:
