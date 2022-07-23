c0d3ae43ad3e500aad6a1ec78b1d5fc68ce13da3
heaseochung <heaseochung@gmail.com>
2022-07-23 18:11:33 +0900

    update train codes for distributed data parrallel

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -1,6 +1,6 @@
 import os
 import hydra
-import logging
+import builtins
 import torch
 import torch.backends.cudnn as cudnn
 from torch import nn
@@ -8,22 +8,27 @@ from torch.utils.data.dataloader import DataLoader
 from torch.nn import functional as F
 import torchvision.utils as vutils
 
-from dataset import Dataset
+import torch.multiprocessing as mp
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("Trainer").setLevel(logging.INFO)
-log = logging.getLogger("Trainer")
+from dataset import Dataset
 
 
 class Trainer:
-    def __init__(self, cfg, gpu) -> None:
+    def __init__(self, gpu, cfg) -> None:
         ### GPU Device setting
         self.gpu = gpu
-        cudnn.benchmark = True
-        cudnn.deterministic = True
+        self.ngpus_per_node = torch.cuda.device_count()
+
+        if cfg.train.ddp.distributed and self.gpu != 0:
+
+            def print_pass(*args):
+                pass
+
+            builtins.print = print_pass
 
         ### Common setting
-        torch.manual_seed(cfg.train.common.seed)
         self.save_path = cfg.train.common.ckpt_dir
         os.makedirs(self.save_path, exist_ok=True)
 
@@ -33,7 +38,8 @@ class Trainer:
         self.end_iters = cfg.train.common.iteration
         self.seed = cfg.train.common.seed
         self.use_wandb = cfg.train.common.use_wandb
-        self.save_every = cfg.train.common.save_every
+        self.save_img_every = cfg.train.common.save_img_every
+        self.save_model_every = cfg.train.common.save_model_every
 
         ### Model setting
         self.generator = None
@@ -44,7 +50,8 @@ class Trainer:
         self.d_optim = None
 
         ### Scheduler setting
-        self.scheduler = None
+        self.g_scheduler = None
+        self.d_scheduler = None
 
         ### Loss setting
         self.l1loss = None
@@ -52,15 +59,18 @@ class Trainer:
         self.gan_loss = None
 
         ### Dataset setting
+        self.distributed = cfg.train.ddp.distributed
         self.dataloader = None
 
         ### Initializer
         self._init_model(cfg.models)
+        if cfg.train.ddp.distributed:
+            self._init_distributed_data_parallel(cfg)
         self._init_optim(cfg.train.optim)
         self._init_loss(cfg.train.loss)
         self._load_state_dict(cfg.models)
         self._init_scheduler(cfg.train.scheduler)
-        self._init_dataset(cfg.train.dataset)
+        self._init_dataset(cfg)
 
         ### Train
         self.train()
@@ -112,54 +122,59 @@ class Trainer:
 
             self.generator = Generator(model.generator).to(self.gpu)
 
-        log.info("Initialized the model")
+        print("Initialized the model")
 
     def _load_state_dict(self, model):
         if model.generator.path:
-            log.info("Train the generator with checkpoint")
-            log.info(f"Loading the checkpoint from : {model.generator.path}")
+            print("Train the generator with checkpoint")
+            print(f"Loading the checkpoint from : {model.generator.path}")
             ckpt = torch.load(
                 model.generator.path, map_location=lambda storage, loc: storage
             )
             if len(ckpt) == 3:
                 self.num_iteration = ckpt["iteration"]
-                self.generator.load_state_dict(ckpt["g"])
+                if isinstance(model, nn.DataParallel):
+                    self.generator.module.load_state_dict(ckpt["g"])
+                else:
+                    self.generator.load_state_dict(ckpt["g"])
                 self.g_optim.load_state_dict(ckpt["g_optim"])
                 self.start_iters = ckpt["iteration"] + 1
             else:
                 self.generator.load_state_dict(ckpt)
         else:
-            log.info("Train the generator without checkpoint")
+            print("Train the generator without checkpoint")
 
         if self.gan_train:
             if model.discriminator.path:
-                log.info("Train the discriminator with checkpoint")
-                log.info(
+                print("Train the discriminator with checkpoint")
+                print(
                     f"Loading the checkpoint from : {model.discriminator.path}"
                 )
+                loc = "cuda:{}".format(self.gpu)
                 ckpt = torch.load(
                     model.discriminator.path,
-                    map_location=lambda storage, loc: storage,
+                    map_location=loc,
                 )
                 if len(ckpt) == 3:
                     self.num_iteration = ckpt["iteration"]
-                    self.discriminator.load_state_dict(ckpt["d"])
+                    if isinstance(model, nn.DataParallel):
+                        self.discriminator.module.load_state_dict(ckpt["d"])
+                    else:
+                        self.discriminator.load_state_dict(ckpt["d"])
                     self.d_optim.load_state_dict(ckpt["d_optim"])
                 else:
                     self.discriminator.load_state_dict(ckpt)
             else:
-                log.info("Train the discriminator without checkpoint")
+                print("Train the discriminator without checkpoint")
 
-        log.info("Initialized the checkpoints")
+        print("Initialized the checkpoints")
 
     def _init_loss(self, losses):
         if losses.lists[0] == "MAE":
-            log.info("Loss for MAE")
             self.l1loss = nn.L1Loss().to(self.gpu)
         elif losses.lists[0] == "Charbonnier":
             from loss import L1_Charbonnier_loss
 
-            log.info("Loss for Charbonnier")
             self.l1loss = L1_Charbonnier_loss().to(self.gpu)
 
         if self.gan_train:
@@ -170,7 +185,7 @@ class Trainer:
                 self.gpu
             )
 
-        log.info("Initialized the losses functions")
+        print("Initialized the losses functions")
 
     def _init_optim(self, optims):
         if optims.type == "Adam":
@@ -178,9 +193,7 @@ class Trainer:
                 self.generator.parameters(),
                 lr=optims.Adam.lr,
                 betas=(optims.Adam.betas[0], optims.Adam.betas[1]),
-                eps=optims.Adam.eps,
                 weight_decay=optims.Adam.weight_decay,
-                amsgrad=optims.Adam.amsgrad,
             )
 
             if self.discriminator:
@@ -188,44 +201,94 @@ class Trainer:
                     self.discriminator.parameters(),
                     lr=optims.Adam.lr,
                     betas=(optims.Adam.betas[0], optims.Adam.betas[1]),
-                    eps=optims.Adam.eps,
                     weight_decay=optims.Adam.weight_decay,
-                    amsgrad=optims.Adam.amsgrad,
                 )
-        log.info("Initialized the optimizers")
+        print("Initialized the optimizers")
 
     def _init_scheduler(self, scheduler):
         if self.start_iters > 0:
-            self.scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            self.g_scheduler = torch.optim.lr_scheduler.MultiStepLR(
                 self.g_optim,
-                scheduler.milestones,
-                scheduler.gamma,
+                scheduler.g_milestones,
+                scheduler.g_gamma,
                 last_epoch=self.start_iters,
             )
+            if self.gan_train:
+                self.d_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                    self.d_optim,
+                    scheduler.d_milestones,
+                    scheduler.d_gamma,
+                    last_epoch=self.start_iters,
+                )
         else:
-            self.scheduler = torch.optim.lr_scheduler.MultiStepLR(
-                self.g_optim, scheduler.milestones, scheduler.gamma
+            self.g_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                self.g_optim, scheduler.g_milestones, scheduler.g_gamma
             )
-        log.info("Initialized the schedulers")
+            if self.gan_train:
+                self.d_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                    self.d_optim, scheduler.d_milestones, scheduler.d_gamma
+                )
+        print("Initialized the schedulers")
 
-    def _init_dataset(self, dataset):
+    def _init_dataset(self, cfg):
         def sample_data(loader):
             while True:
                 for batch in loader:
                     yield batch
 
+        cfg.train.dataset.num_workers = 4 * torch.cuda.device_count()
+        train_dataset = Dataset(cfg.train.dataset)
+
+        if self.distributed:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(
+                train_dataset,
+                num_replicas=cfg.train.ddp.world_size,
+                rank=cfg.train.ddp.rank,
+            )
+        else:
+            train_sampler = None
+
         dataloader = DataLoader(
-            dataset=Dataset(dataset),
-            batch_size=dataset.batch_size,
-            shuffle=True,  # TODO None for now, this will be changed when ddp is applied
-            num_workers=dataset.num_workers,
+            dataset=train_dataset,
+            batch_size=cfg.train.dataset.batch_size,
+            shuffle=(train_sampler is None),
+            num_workers=cfg.train.dataset.num_workers,
             pin_memory=True,
-            sampler=None,  # TODO None for now, this will be changed when ddp is applied
+            sampler=train_sampler,
             drop_last=True,
         )
         self.dataloader = sample_data(dataloader)
 
-        log.info("Initialized the datasets")
+        print("Initialized the datasets")
+
+    def _init_distributed_data_parallel(self, cfg):
+        cfg.train.ddp.rank = cfg.train.ddp.nr * cfg.train.ddp.gpus + self.gpu
+
+        dist.init_process_group(
+            backend=cfg.train.ddp.dist_backend,
+            init_method=cfg.train.ddp.dist_url,
+            world_size=cfg.train.ddp.world_size,
+            rank=cfg.train.ddp.rank,
+        )
+
+        torch.cuda.set_device(self.gpu)
+        self.generator.to(self.gpu)
+
+        cfg.train.dataset.batch_size = int(
+            cfg.train.dataset.batch_size / self.ngpus_per_node
+        )
+
+        self.generator = torch.nn.parallel.DistributedDataParallel(
+            self.generator, device_ids=[self.gpu]
+        )
+
+        if self.gan_train:
+            self.discriminator.to(self.gpu)
+            self.discriminator = torch.nn.parallel.DistributedDataParallel(
+                self.discriminator, device_ids=[self.gpu]
+            )
+
+        print("Initialized the Distributed Data Parallel")
 
     def train_psnr(self):
         self.generator.train()
@@ -241,7 +304,7 @@ class Trainer:
             self.generator.zero_grad()
             loss.backward()
             self.g_optim.step()
-            self.scheduler.step()
+            self.g_scheduler.step()
 
             results = torch.cat(
                 (
@@ -254,19 +317,26 @@ class Trainer:
                 2,
             )
 
-            vutils.save_image(
-                results, os.path.join(self.save_path, f"preds.jpg")
-            )
+            if self.gpu == 0:
+                if i % self.save_img_every == 0:
+                    vutils.save_image(
+                        results, os.path.join(self.save_path, f"preds.jpg")
+                    )
 
-            if i % self.save_every == 0:
-                torch.save(
-                    {
-                        "g": self.generator.state_dict(),
-                        "g_optim": self.g_optim.state_dict(),
-                        "iteration": i,
-                    },
-                    os.path.join(self.save_path, f"{str(i).zfill(6)}.pth"),
-                )
+                if i % self.save_model_every == 0:
+                    if isinstance(self.generator, nn.DataParallel):
+                        g_state_dict = self.generator.module.state_dict()
+                    else:
+                        g_state_dict = self.generator.state_dict()
+
+                    torch.save(
+                        {
+                            "g": g_state_dict,
+                            "g_optim": self.g_optim.state_dict(),
+                            "iteration": i,
+                        },
+                        os.path.join(self.save_path, f"{str(i).zfill(6)}.pth"),
+                    )
 
     def train_gan(self):
         self.generator.train()
@@ -314,7 +384,8 @@ class Trainer:
             g_loss.backward()
             self.g_optim.step()
 
-            self.scheduler.step()
+            self.g_scheduler.step()
+            self.d_scheduler.step()
 
             results = torch.cat(
                 (
@@ -327,28 +398,40 @@ class Trainer:
                 2,
             )
 
-            vutils.save_image(
-                results, os.path.join(self.save_path, f"preds.jpg")
-            )
+            if self.gpu == 0:
+                if i % self.save_img_every == 0:
+                    vutils.save_image(
+                        results, os.path.join(self.save_path, f"preds.jpg")
+                    )
+                if i % self.save_model_every == 0:
+                    if isinstance(self.generator, nn.DataParallel):
+                        g_state_dict = self.generator.module.state_dict()
+                        d_state_dict = self.discriminator.module.state_dict()
+                    else:
+                        g_state_dict = self.generator.state_dict()
+                        d_state_dict = self.discriminator.state_dict()
 
-            if i % self.save_every == 0:
-                torch.save(
-                    {
-                        "g": self.generator.state_dict(),
-                        "g_optim": self.g_optim.state_dict(),
-                        "iteration": i,
-                    },
-                    os.path.join(self.save_path, f"g_{str(i).zfill(6)}.pth"),
-                )
+                    torch.save(
+                        {
+                            "g": g_state_dict,
+                            "g_optim": self.g_optim.state_dict(),
+                            "iteration": i,
+                        },
+                        os.path.join(
+                            self.save_path, f"g_{str(i).zfill(6)}.pth"
+                        ),
+                    )
 
-                torch.save(
-                    {
-                        "d": self.discriminator.state_dict(),
-                        "d_optim": self.d_optim.state_dict(),
-                        "iteration": i,
-                    },
-                    os.path.join(self.save_path, f"d_{str(i).zfill(6)}.pth"),
-                )
+                    torch.save(
+                        {
+                            "d": d_state_dict,
+                            "d_optim": self.d_optim.state_dict(),
+                            "iteration": i,
+                        },
+                        os.path.join(
+                            self.save_path, f"d_{str(i).zfill(6)}.pth"
+                        ),
+                    )
 
     def train(self):
         if not self.gan_train:
@@ -359,8 +442,26 @@ class Trainer:
 
 @hydra.main(config_path="../configs/", config_name="train.yaml")
 def main(cfg):
-    Trainer(cfg, 0)
+    cudnn.benchmark = True
+    cudnn.deterministic = True
+    torch.manual_seed(cfg.train.common.seed)
+
+    if torch.cuda.device_count() > 1:
+        print("Train with multiple GPUs")
+        cfg.train.ddp.distributed = True
+        gpus = torch.cuda.device_count()
+        cfg.train.ddp.world_size = gpus * cfg.train.ddp.nodes
+        mp.spawn(
+            Trainer,
+            nprocs=gpus,
+            args=(cfg,),
+        )
+        dist.destroy_process_group()
+    else:
+        print("Train with single GPUs")
+        Trainer(0, cfg)
 
 
+# 1736225 using degradation for super-resolution scunet
 if __name__ == "__main__":
     main()
